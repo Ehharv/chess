faa56995e1bb8e5695d060d1f8970487a6602d7b
Elle Kelsey <eh448@byu.edu>
2024-08-01 14:35:35 -0600

    start prelogin ui and create state enum

--- client/src/main/java/ui/PreloginUi.java
+++ client/src/main/java/ui/PreloginUi.java
@@ -1,4 +1,43 @@
 package ui;
 
+import java.util.Arrays;
+
 public class PreloginUi {
+    private State state = State.SIGNEDOUT;
+    private ServerFacade server;
+    private String serverUrl;
+
+    public PreloginUi(String serverUrl) {
+        this.serverUrl = serverUrl;
+        server = new ServerFacade(serverUrl);
+    }
+
+    public String help(){
+        return """
+        register <username> <password> <email> (to create a new account)
+        login <username> <password> (to an existing account)
+        quit (to exit)
+        help (list available commands)
+        """;
+    }
+
+    public String eval(String input){
+        try{
+            var formatedIn = input.toLowerCase().split(" ");
+            var cmd = (formatedIn.length > 0) ? formatedIn[0] : "help";
+            var params = Arrays.copyOfRange(formatedIn, 1, formatedIn.length);
+
+            return switch (cmd){
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> "quit";
+                default -> "help";
+            };
+        } catch (Exception e){
+            return e.getMessage();
+        }
+    }
+
+
+
 }
