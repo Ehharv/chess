44997db72a224ef5c2f0977d9a673e3a7cbd3832
Elle Kelsey <eh448@byu.edu>
2024-08-01 15:07:34 -0600

    create login and register methods for ui

--- client/src/main/java/ui/PreloginUi.java
+++ client/src/main/java/ui/PreloginUi.java
@@ -1,5 +1,8 @@
 package ui;
 
+import model.UserData;
+import model.returnobjects.AuthTokenResponse;
+
 import java.util.Arrays;
 
 public class PreloginUi {
@@ -23,8 +26,11 @@ public class PreloginUi {
 
     public String eval(String input){
         try{
+            // make input lowercase
             var formatedIn = input.toLowerCase().split(" ");
+            // if there isnt input, change it to help
             var cmd = (formatedIn.length > 0) ? formatedIn[0] : "help";
+            // make an array of the args
             var params = Arrays.copyOfRange(formatedIn, 1, formatedIn.length);
 
             return switch (cmd){
@@ -38,6 +44,27 @@ public class PreloginUi {
         }
     }
 
+    private String register(String[] params) throws Exception {
+        if(params.length != 3) {
+            throw new Exception("Expected: <username> <password> <email>");
+        } else{
+            UserData user = new UserData(params[0], params[1], params[2]);
+            AuthTokenResponse authToken = server.register(user);
+            state = State.SIGNEDIN;
+            return authToken.toString();
+        }
+    }
+
+    private String login(String[] params) throws Exception {
+        if(params.length != 2) {
+            throw new Exception("Expected: <username> <password>");
+        } else {
+            UserData user = new UserData(params[0], params[1], null);
+            AuthTokenResponse authToken = server.login(user);
+            state = State.SIGNEDIN;
+            return authToken.toString();
+        }
+    }
 
 
 }
