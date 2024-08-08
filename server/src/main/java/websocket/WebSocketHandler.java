a2cdc634f7ae49f162fd330639c06da1463b6477
Elle Kelsey <eh448@byu.edu>
2024-08-08 09:49:50 -0600

    stub websocket handler

---
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -0,0 +1,42 @@
+package websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.*;
+
+@WebSocket
+public class WebSocketHandler {
+
+    @OnWebSocketConnect
+    public void onConnect(Session session) {}
+
+    @OnWebSocketClose
+    public void onClose(Session session) {}
+
+    @OnWebSocketError
+    public void onError(Throwable error) {}
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) {
+        // determine message type
+        // call a method to process
+    }
+
+    // call service and/or send message to clients
+    private void connect(String message){}
+
+    // service.method(...)
+    private void makeMove(String message){}
+
+    // sendMessage(...)
+    private void leaveGame(String message){}
+
+    // broadcastMessage(...)
+    private void resignGame(String message){}
+
+    // send a message
+    private void sendMessage(String message, Session session){}
+
+    // broadcast to all but specified
+    private void broadcastMessage(int gameId, String message, Session exceptThisSession){}
+
+}
