166cf52f8203002ea8108f3850ab429338186bb1
Elle Kelsey <eh448@byu.edu>
2024-08-08 10:11:41 -0600

    write onMessage

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,11 +1,14 @@
 package websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.*;
+import websocket.commands.UserGameCommand;
 
 @WebSocket
 public class WebSocketHandler {
     private final WebSocketSessions sessions = new WebSocketSessions();
+    private final Gson gson = new Gson();
 
     @OnWebSocketConnect
     public void onConnect(Session session) {}
@@ -19,11 +22,21 @@ public class WebSocketHandler {
     @OnWebSocketMessage
     public void onMessage(Session session, String message) {
         // determine message type
+        UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
+        UserGameCommand.CommandType commandType = command.getCommandType();
         // call a method to process
+        switch (commandType) {
+            case LEAVE -> leaveGame(message);
+            case RESIGN -> resignGame(message);
+            case CONNECT -> connect(message);
+            case MAKE_MOVE -> makeMove(message);
+        }
     }
 
     // call service and/or send message to clients
-    private void connect(String message){}
+    private void connect(String message){
+
+    }
 
     // service.method(...)
     private void makeMove(String message){}
